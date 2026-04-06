99a56799beb981f57a3b40ef0b4aff34331c6bc0
Rubens Soares <rubens.soares@liferay.com>
2026-04-06 09:29:14 -0300

    ECU-16068 Migrate SourceFormatterRunner to Step architecture

--- src/main/java/com/liferay/upgrades/project/dependency/sourceformatter/SourceFormatterRunner.java
+++ src/main/java/com/liferay/upgrades/project/dependency/sourceformatter/SourceFormatterRunner.java
@@ -1,11 +1,18 @@
 package com.liferay.upgrades.project.dependency.sourceformatter;
 
+import com.liferay.upgrades.project.dependency.Step;
+import com.liferay.upgrades.project.dependency.model.Context;
+import com.liferay.upgrades.project.dependency.model.VersionOptions;
+
 import java.io.File;
 import java.util.logging.Logger;
 
-public class SourceFormatterRunner {
+public class SourceFormatterRunner implements Step {
 
-    public void run(String directory) throws Exception {
+    @Override
+    public Context applyChanges(VersionOptions stepOptions) throws Exception {
+        String directory = stepOptions.directory;
+
         _log.info("Running source-formatter automation...");
 
         String modulesDirectory = directory + File.separator + "modules";
@@ -15,15 +22,33 @@ public class SourceFormatterRunner {
         if (!modulesFolder.exists() || !modulesFolder.isDirectory()) {
             _log.warning("Modules directory not found at: " + modulesDirectory);
 
-            return;
+            return null;
         }
 
-        String command = "blade gw formatSource -Pjava.parser.enabled=false -Psource.check.category.names=Upgrade -Psource.file.extensions=gradle,xml,bnd";
+        String command =
+            "blade gw formatSource -Pjava.parser.enabled=false " +
+                "-Psource.check.category.names=Upgrade " +
+                "-Psource.file.extensions=gradle,xml,bnd";
 
         _executeShell(modulesDirectory, command);
+
+        return new Context(
+            stepOptions.ticket, null, directory, null, null, null, null, null);
     }
 
-    private void _executeShell(String directory, String command) throws Exception {
+    @Override
+    public String commitMessage(Context context) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(context.ticket());
+        sb.append(" SF automation update dependencies");
+
+        return sb.toString();
+    }
+
+    private void _executeShell(String directory, String command)
+        throws Exception {
+
         ProcessBuilder processBuilder = new ProcessBuilder("sh", "-c", command);
 
         processBuilder.directory(new File(directory));
@@ -35,11 +60,15 @@ public class SourceFormatterRunner {
         int exitCode = process.waitFor();
 
         if (exitCode != 0) {
-            _log.warning("Source formatter command failed with exit code: " + exitCode);
-        } else {
+            _log.warning(
+                "Source formatter command failed with exit code: " + exitCode);
+        }
+        else {
             _log.info("Successfully ran source formatter.");
         }
     }
 
-    private static final Logger _log = Logger.getLogger(SourceFormatterRunner.class.getName());
+    private static final Logger _log = Logger.getLogger(
+        SourceFormatterRunner.class.getName());
+
 }
