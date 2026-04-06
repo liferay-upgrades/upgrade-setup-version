89d5ec5d74ba536dd4b34a3d6f4dc1ec38946d30
Rubens Soares <rubens.soares@liferay.com>
2026-04-06 09:22:01 -0300

    ECU-16068 Migrate BndRefactorer to Step architecture

--- src/main/java/com/liferay/upgrades/project/dependency/bnd/BndRefactorer.java
+++ src/main/java/com/liferay/upgrades/project/dependency/bnd/BndRefactorer.java
@@ -1,54 +1,85 @@
 package com.liferay.upgrades.project.dependency.bnd;
 
+import com.liferay.upgrades.project.dependency.Step;
+import com.liferay.upgrades.project.dependency.model.Context;
+import com.liferay.upgrades.project.dependency.model.VersionOptions;
+
 import java.io.File;
 import java.util.logging.Logger;
 
-public class BndRefactorer {
+public class BndRefactorer implements Step {
 
-
-    public boolean run(String directory) throws Exception {
+    @Override
+    public Context applyChanges(VersionOptions stepOptions) throws Exception {
+        String directory = stepOptions.directory;
         String os = System.getProperty("os.name").toLowerCase();
         String sedCommand;
 
         if (!_hasBundleVersionConstraints(directory)) {
-            _log.info("No 'bundle-version' attribute constraints found in .bnd files. Skipping.");
-            return false;
+            _log.info(
+                "No 'bundle-version' attribute constraints found in .bnd " +
+                    "files. Skipping.");
+
+            return null;
         }
 
         if (os.contains("linux")) {
             _log.info("Linux/Unix detected for .bnd refactor.");
 
             sedCommand = "sed -i -E 's/;?bundle-version=\"[^\"]+\"//g'";
-        } else if (os.contains("mac")) {
+        }
+        else if (os.contains("mac")) {
             _log.info("Mac OS detected for .bnd refactor.");
 
             sedCommand = "sed -i '' -E 's/;?bundle-version=\"[^\"]+\"//g'";
-        } else {
-            throw new UnsupportedOperationException("OS not supported for .bnd refactor: " + os);
+        }
+        else {
+            throw new UnsupportedOperationException(
+                "OS not supported for .bnd refactor: " + os);
         }
 
         String command = String.format(
-                "find . -type d \\( -name .gradle -o -name build -o -name .git \\) -prune -o -type f -name \"*.bnd\" -print | while read -r file; do %s \"$file\"; done",
-                sedCommand
-        );
+            "find . -type d \\( -name .gradle -o -name build -o -name .git " +
+                "\\) -prune -o -type f -name \"*.bnd\" -print | while read " +
+                "-r file; do %s \"$file\"; done",
+            sedCommand);
 
         _executeShell(directory, command);
-        return true;
+
+        return new Context(
+            stepOptions.ticket, null, directory, null, null, null, null, null);
     }
 
-    private boolean _hasBundleVersionConstraints(String directory) throws Exception {
-        String checkCommand = "grep -rqlE --include=\"*.bnd\" --exclude-dir={.gradle,build,.git} \"bundle-version=\\\"\" .";
+    @Override
+    public String commitMessage(Context context) {
+        StringBuilder sb = new StringBuilder();
 
-        ProcessBuilder processBuilder = new ProcessBuilder("sh", "-c", checkCommand);
+        sb.append(context.ticket());
+        sb.append(" Refactor bnd.bnd: remove hardcoded bundle-version constraints");
+
+        return sb.toString();
+    }
+
+    private boolean _hasBundleVersionConstraints(String directory)
+        throws Exception {
+
+        String checkCommand =
+            "grep -rqlE --include=\"*.bnd\" --exclude-dir={.gradle,build,.git} " +
+                "\"bundle-version=\\\"\" .";
+
+        ProcessBuilder processBuilder = new ProcessBuilder(
+            "sh", "-c", checkCommand);
 
         processBuilder.directory(new File(directory));
 
         Process process = processBuilder.start();
+
         return process.waitFor() == 0;
     }
 
+    private void _executeShell(String directory, String command)
+        throws Exception {
 
-    private void _executeShell(String directory, String command) throws Exception {
         ProcessBuilder processBuilder = new ProcessBuilder("sh", "-c", command);
 
         processBuilder.directory(new File(directory));
@@ -60,12 +91,15 @@ public class BndRefactorer {
         int exitCode = process.waitFor();
 
         if (exitCode != 0) {
-            _log.warning("BND Refactor command failed with exit code: " + exitCode);
-        } else {
+            _log.warning(
+                "BND Refactor command failed with exit code: " + exitCode);
+        }
+        else {
             _log.info("Successfully refactored bnd.bnd files.");
         }
-
     }
 
-    private static final Logger _log = Logger.getLogger(BndRefactorer.class.getName());
+    private static final Logger _log = Logger.getLogger(
+        BndRefactorer.class.getName());
+
 }
