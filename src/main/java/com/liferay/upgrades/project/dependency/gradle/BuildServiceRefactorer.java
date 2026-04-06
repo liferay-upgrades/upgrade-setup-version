00e3278834b47bac4e22860a7ad531d322457a7c
Rubens Soares <rubens.soares@liferay.com>
2026-04-06 09:43:54 -0300

    ECU-16068 Migrate BuildServiceRefactorer to Step architecture

--- src/main/java/com/liferay/upgrades/project/dependency/gradle/BuildServiceRefactorer.java
+++ src/main/java/com/liferay/upgrades/project/dependency/gradle/BuildServiceRefactorer.java
@@ -1,13 +1,52 @@
 package com.liferay.upgrades.project.dependency.gradle;
 
+import com.liferay.upgrades.project.dependency.Step;
+import com.liferay.upgrades.project.dependency.git.GitHandler;
+import com.liferay.upgrades.project.dependency.model.Context;
+import com.liferay.upgrades.project.dependency.model.VersionOptions;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
-public class BuildServiceRefactorer {
+public class BuildServiceRefactorer implements Step {
 
-    public List<File> findServiceModules(String directory) {
+    @Override
+    public Context applyChanges(VersionOptions stepOptions) throws Exception {
+        return null;
+    }
+
+    @Override
+    public String commitMessage(Context context) {
+        return "";
+    }
+
+    @Override
+    public Context run(VersionOptions stepOptions) throws Exception {
+        String directory = stepOptions.directory;
+
+        List<File> serviceModules = _findServiceModules(directory);
+
+        GitHandler gitHandler = new GitHandler();
+
+        for (File module : serviceModules) {
+            _runBuildService(module.getAbsolutePath());
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.append(stepOptions.ticket);
+            sb.append(" buildService in ");
+            sb.append(module.getName());
+            sb.append(" module");
+
+            gitHandler.commit(directory, sb.toString());
+        }
+
+        return null;
+    }
+
+    private List<File> _findServiceModules(String directory) {
         List<File> serviceModules = new ArrayList<>();
 
         File modulesDir = new File(directory, "modules");
@@ -22,28 +61,37 @@ public class BuildServiceRefactorer {
     private void _findServiceXml(File directory, List<File> serviceModules) {
         String name = directory.getName();
 
-        if (name.equals("bin") || name.equals("build") || name.equals(".gradle") || name.equals(".idea")) {
+        if (name.equals("bin") || name.equals("build") ||
+            name.equals(".gradle") || name.equals(".idea")) {
+
             return;
         }
 
         File[] files = directory.listFiles();
-        if (files == null) return;
+
+        if (files == null) {
+            return;
+        }
 
         for (File file : files) {
             if (file.isDirectory()) {
                 _findServiceXml(file, serviceModules);
-            } else if (file.getName().equals("service.xml")) {
+            }
+            else if (file.getName().equals("service.xml")) {
                 serviceModules.add(file.getParentFile());
             }
         }
     }
 
-    public void run(String modulePath) throws Exception {
+    private void _runBuildService(String modulePath) throws Exception {
         _log.info("Running buildService in " + modulePath);
+
         _executeShell(modulePath, "blade gw buildService");
     }
 
-    private void _executeShell(String directory, String command) throws Exception {
+    private void _executeShell(String directory, String command)
+        throws Exception {
+
         ProcessBuilder processBuilder = new ProcessBuilder("sh", "-c", command);
 
         processBuilder.directory(new File(directory));
@@ -55,9 +103,12 @@ public class BuildServiceRefactorer {
         int exitCode = process.waitFor();
 
         if (exitCode != 0) {
-            _log.warning("Command '" + command + "' failed with exit code: " + exitCode);
+            _log.warning(
+                "Command '" + command + "' failed with exit code: " + exitCode);
         }
     }
 
-    private static final Logger _log = Logger.getLogger(BuildServiceRefactorer.class.getName());
+    private static final Logger _log = Logger.getLogger(
+        BuildServiceRefactorer.class.getName());
+
 }
